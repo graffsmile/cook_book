65a342c8299229cf23c8e03a46c15b1d0724dedd
graff <sevulybin@yandex.ru>
2024-10-13 18:16:58 +0500

    Change list_products.py

--- list_products.py
+++ list_products.py
@@ -16,6 +16,12 @@ with open('recipes.txt', encoding='utf-8') as file:
 # Список продуктов на несколько персон
 
 def get_shop_list_by_dishes(person_count: int, dishes: list):
+    """ Функция, которая на вход принимает список блюд из cook_book и количество персон для кого мы будем готовить.
+    На выходе получается словарь с названием ингредиентов и его количестом для блюда.
+    Если блюда нет в cook_book выводит сообщение о том что такого блюда нет в кулинарной книге.
+    
+    """
+
     result = {}
     for dish in dishes:
         if dish in cook_book:
@@ -23,6 +29,6 @@ def get_shop_list_by_dishes(person_count: int, dishes: list):
                 if ingredients['ingredient_name'] not in result:
                     result[ingredients['ingredient_name']] = {'measure': ingredients['measure'], 'quantity': (int(ingredients['quantity']) * person_count)}
         else:
-             print(f'{dish} - такого блюда нет в книге')
+             print(f'{dish} - такого блюда нет в кулинарной книге')
     print(result)
 get_shop_list_by_dishes(2, ['Запеченный картофель', 'Омлет'])
